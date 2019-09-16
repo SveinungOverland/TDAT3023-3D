2d3d1467f5f456010e94a7d5292fbae140ec02c1
SveinungOverland <sveinungg.overlandd@gmail.com>
2019-09-17 01:10:16 +0200

    321321

---
+++ Unity/Planetary System 2.0/Assets/ComponentGravity.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComponentGravity : MonoBehaviour
+{
+    public float G = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate() {
+        var objects = GameObject.FindGameObjectsWithTag("Body");
+        for (int i = 0; i < objects.Length; i++)
+        {
+            var x = objects[i];
+            var xTransform = x.GetComponent<Transform>();
+            var xRigid = x.GetComponent<Rigidbody>();
+            for (int j = 0; j < objects.Length; j++)
+            {
+                if (i != j)
+                {
+                    var y = objects[j];
+                    var yTransform = y.GetComponent<Transform>();
+                    var yRigid = y.GetComponent<Rigidbody>();
+                    var xToY = yTransform.position - xTransform.position;
+                    var force = G * (xRigid.mass * yRigid.mass) / System.Math.Pow(xToY.magnitude, 2);
+                    xRigid.AddForce(xToY.normalized * (float)force);
+                }
+            }
+        }
+    }
+}
